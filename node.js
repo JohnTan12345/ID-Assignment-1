18626835886c0a7834494870b68f5ba5c6c90fee
John Tan <s10259175@connect.np.edu.sg>
2024-11-28 15:42:02 +0800

    automated the dislayed game, removed sticky navbar js (unnecessary)

--- node.js
+++ node.js
@@ -1,12 +1,48 @@
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
+const displaygames = document.getElementById("displaygames");
 
-function stickynavbar() {
-    if (window.scrollY >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
+function getgames() {
+    let gameslist = []
+    return gameslist
 }
 
-window.onscroll = stickynavbar()
+function creategameinfo() {
+    let displayamount = displaygames.dataset.displayamount > 0 ? displaygames.dataset.displayamount : gameslist.length
+    let gameslist = getgames()
+
+    for (i = 0; i < displayamount; i++) {
+
+        let game
+
+        /* Check if there is an available game, else default */
+        gameslist[i] != undefined ? game = gameslist[i] : game = {"name": "default", "active": 0, "likes": 0, "dislikes": 0}
+
+        let name = game.name
+        let active = game.active
+        let likes = game.likes
+        let total = game.likes + game.dislikes
+
+        let background = document.createElement("div")
+        displaygames.appendChild(background)
+        background.className = "game"
+
+        let thumbnail = document.createElement("img")
+        background.appendChild(thumbnail)
+        thumbnail.src = "img/gamethumbnails/" + name + ".png"
+        thumbnail.alt = `${name}'s thumbnail`
+        thumbnail.className = "gamethumbnail"
+
+        let gamename = document.createElement("strong")
+        gamename.innerHTML = name
+        background.appendChild(gamename)
+
+        let activeplayers = document.createElement("strong")
+        activeplayers.innerHTML = active
+        background.appendChild(activeplayers)
+
+        let ratio = document.createElement("strong")
+        ratio.innerHTML = `${likes}:${total}`
+        background.appendChild(ratio)
+    }
+
+}
+creategameinfo()
